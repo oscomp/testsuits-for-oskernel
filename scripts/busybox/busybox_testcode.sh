ac8885cf186fb007fdf6201552e9eff912d0d318
Mingjian W <wmj@alphamj.cn>
2021-07-18 10:56:40 +0800

    使用busybox执行cat

--- scripts/busybox/busybox_testcode.sh
+++ scripts/busybox/busybox_testcode.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-RST=result.txt
-if [ -f $RST ];then
-	rm $RST
-fi
-touch $RST
+# RST=result.txt
+# if [ -f $RST ];then
+# 	rm $RST
+# fi
+# touch $RST
 
-echo "If the CMD runs incorrectly, return value will put in $RST" > $RST
-echo -e "Else nothing will put in $RST\n" >> $RST
-echo "TEST START" >> $RST
+# echo "If the CMD runs incorrectly, return value will put in $RST" > $RST
+# echo -e "Else nothing will put in $RST\n" >> $RST
+# echo "TEST START" >> $RST
 
-cat ./busybox_cmd.txt | while read line
+./busybox cat ./busybox_cmd.txt | while read line
 do
 	eval "./busybox $line"
 	RTN=$?
 	if [[ $RTN -ne 0 && $line != "false" ]] ;then
 		echo "testcase busybox $line fail"
-		echo "return: $RTN, cmd: $line" >> $RST
+		# echo "return: $RTN, cmd: $line" >> $RST
 	else
 		echo "testcase busybox $line success"
 	fi
 done
 
-echo "TEST END" >> $RST
+# echo "TEST END" >> $RST
