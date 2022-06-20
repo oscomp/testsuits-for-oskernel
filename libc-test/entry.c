daacf4d157fce27c485177d9a700b9780cbfb818
w-mj <wmj@alphamj.cn>
2022-06-20 08:14:57 +0000

    add libc-test

---
+++ libc-test/entry.c
@@ -0,0 +1,24 @@
+#include "entry.h"
+
+
+static int mystrcmp(const char *s1, const char *s2) {
+    while (*s1 != *s2) {
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        return -1;
+    }
+    int i = 0;
+    while(table[i].func) {
+        if (mystrcmp(table[i].name, argv[1]) == 0) {
+            return table[i].func(argc - 1, argv + 1);
+        }
+        i++;
+    }
+    return -1;
+}
