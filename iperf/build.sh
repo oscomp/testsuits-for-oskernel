bde3e8d5562e22d6a74bd5b781f7075e66cba11a
Zhouyi Zhou <zhouzhouyi@gmail.com>
2025-05-28 17:54:13 +0800

    link iperf statically

--- iperf/build.sh
+++ iperf/build.sh
@@ -4,15 +4,15 @@ make clean
 
 if [[ "$1" == *"riscv"* ]]; then
     if [[ "$1" == *"musl"* ]]; then
-        ./configure  --prefix=$1 --host=riscv64-linux-musl CC=riscv64-buildroot-linux-musl-gcc
+        ./configure  --prefix=$1 --host=riscv64-linux-musl CC=riscv64-buildroot-linux-musl-gcc  --enable-static-bin
     else
-        ./configure  --prefix=$1 --host=riscv64-linux-gnu CC=riscv64-linux-gnu-gcc
+        ./configure  --prefix=$1 --host=riscv64-linux-gnu CC=riscv64-linux-gnu-gcc  --enable-static-bin
     fi
 else
     if [[ "$1" == *"musl"* ]]; then
-        ./configure  --prefix=$1 --host=loongarch64-linux-musl CC=loongarch64-linux-musl-gcc
+        ./configure  --prefix=$1 --host=loongarch64-linux-musl CC=loongarch64-linux-musl-gcc  --enable-static-bin
     else
-        ./configure  --prefix=$1 --host=loongarch64-linux-gnu CC=loongarch64-linux-gnu-gcc
+        ./configure  --prefix=$1 --host=loongarch64-linux-gnu CC=loongarch64-linux-gnu-gcc  --enable-static-bin
     fi
 fi
 
