904ae05c866816cb867ad346f9400299eb6cab49
Zhouyi Zhou <zhouzhouyi@gmail.com>
2025-07-18 11:25:52 +0800

    Add test scripts for interrupt test

---
+++ judges/judge_interrupts-test-glibc.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+
+def parse_interrupt_test(output):
+    ans = {}
+    key = ""
+    for line in output.split("\n"):
+        if "START interrupts-test" in line:
+            key = "interrupts-test " + line.split(" ")[7].strip('####')
+        print(key)    
+        if "passed" in line and "interrupts-test 1" in key:
+            ans[key] = 15
+        if "passed" in line and "interrupts-test 2" in key:
+            ans[key] = 15
+
+    return ans
+
+serial_out = sys.stdin.read()
+
+interrupt_output = parse_interrupt_test(serial_out)
+
+
+results = [{
+    "name": k,
+    "pass": 1,
+    "total": 1,
+    "score": v,
+} for k, v in interrupt_output.items()]
+print(json.dumps(results))
---
+++ scripts/interrupts-test.sh
@@ -0,0 +1,7 @@
+./busybox echo "#### OS COMP TEST GROUP START interrupts-test 1####"
+./interrupts-test-1
+./busybox echo "#### OS COMP TEST GROUP END interrupts-test 1####"
+
+./busybox echo "#### OS COMP TEST GROUP START interrupts-test 2####"
+./interrupts-test-2
+./busybox echo "#### OS COMP TEST GROUP END interrupts-test 2####"
