9654dff9f2d2269d3674c17ba52d5efbb5daf723
Zhouyi Zhou <zhouzhouyi@gmail.com>
2025-07-30 10:56:49 +0800

    fix some bugs in git test script

--- scripts/git_testcode.sh
+++ scripts/git_testcode.sh
@@ -1,7 +1,11 @@
 ./busybox echo "#### OS COMP TEST GROUP START git ####"
+./usr/bin/git config --global --add safe.directory "$(pwd)"
+./usr/bin/git config --global user.email "you@example.com"
+./usr/bin/git config --global user.name "Your Name"
 ./usr/bin/git help
 ./usr/bin/git init
-./busybox cat ""  > README.md
+./busybox echo "hello world" > README.md
+./usr/bin/git add README.md
 ./usr/bin/git commit -m "add README.md"
 ./usr/bin/git log
 ./busybox echo "#### OS COMP TEST GROUP END git ####"
